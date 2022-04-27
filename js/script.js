9f0d8d870787a7087d43dc9630d65fcd73812b7d
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-04-27 17:18:14 +0000

    Added program

--- js/script.js
+++ js/script.js
@@ -7,14 +7,34 @@
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/<RESPITORY>/sw.js", {
-    scope: "/<RESPITORY>/",
+  navigator.serviceWorker.register("/ICS2O-Assignment5-HTML/sw.js", {
+    scope: "/ICS2O-Assignment5-HTML/",
   })
 }
 
 /**
- * Alerts "Hello, World!"
+ * Finds the value of pi using the user's number of iterations.
  */
 function onButtonClick() {
-  alert("Hello, World!")
+  var numberOfIterations = document.getElementById("number-of-iterations").value
+  var currentAction = "minus"
+  var number = 1
+  var pi
+  
+  pi = (4 / number)
+  while (numberOfIterations > 1) {
+    if (currentAction == "minus") {
+      number = number + 2
+      pi = pi - (4 / number)
+      currentAction = "plus"
+    } else if (currentAction == "plus") {
+      number = number + 2
+      pi = pi + (4 / number)
+      currentAction = "minus"
+    }
+    numberOfIterations = numberOfIterations - 1
+  }
+  currentAction == "minus"
+  number = 1
+  document.getElementById("result").innerHTML = "<p>π is equal to " + pi + "</p>"
 }
